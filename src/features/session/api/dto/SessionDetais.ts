a7e7304aee35e2542ccc909b4b934ec3846aceab
nikita <nikita.lavrentyev@mysky.com>
2024-08-30 11:46:10 +0400

    fix GetAllDevicesQuery

---
+++ src/features/session/api/dto/SessionDetais.ts
@@ -0,0 +1,10 @@
+export class SessionDetails {
+  id: number;
+  userId: string;
+  ip: string;
+  title: string;
+  last_active_date: Date;
+  token_issue_date: Date;
+  token_expiration_date: Date;
+  device_id: string;
+}
--- src/features/session/api/dto/output/allDevices.output.dto.ts
+++ src/features/session/api/dto/output/allDevices.output.dto.ts
@@ -1,4 +1,5 @@
 import { SessionDocument } from '@features/session/domain/session-mongo.entity';
+import { SessionDetails } from '@features/session/api/dto/SessionDetais';
 
 export class AllDevicesOutputDto {
   ip: string;
@@ -10,14 +11,14 @@ export class AllDevicesOutputDto {
 // MAPPERS
 
 export const AllDevicesOutputDtoMapper = (
-  session: SessionDocument,
+  session: SessionDetails,
 ): AllDevicesOutputDto => {
   const outputDto = new AllDevicesOutputDto();
 
   outputDto.ip = session.ip;
   outputDto.title = session.title;
-  outputDto.lastActiveDate = session.lastActiveDate;
-  outputDto.deviceId = session.deviceId;
+  outputDto.lastActiveDate = session.last_active_date.toISOString();
+  outputDto.deviceId = session.device_id;
 
   return outputDto;
 };
